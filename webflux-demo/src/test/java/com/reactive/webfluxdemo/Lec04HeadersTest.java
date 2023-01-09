63e5a7402510da4a9bbf83b5700d4e47f8ee056f
Julio Cesar Chacon Vilela <julio.chacon@neoris.com>
2023-01-09 10:23:23 -0500

    Fin Sec05 - WebClient

---
+++ webflux-demo/src/test/java/com/reactive/webfluxdemo/Lec04HeadersTest.java
@@ -0,0 +1,45 @@
+package com.reactive.webfluxdemo;
+
+import com.reactive.webfluxdemo.dto.MultiplyRequestDto;
+import com.reactive.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec04HeadersTest extends BaseTest {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void headersTest() {
+
+        Mono<Response> responseMono = webClient
+                .post()
+                .uri("reactive-math/multiply")
+                .bodyValue(buildRequestDto(5, 2))
+                .headers(h -> h.set("some-key", "some-value"))
+                .retrieve()
+                .bodyToMono(Response.class)
+                .doOnNext(System.out::println);
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1)
+                .verifyComplete();
+
+    }
+
+    private MultiplyRequestDto buildRequestDto(int a, int b) {
+        MultiplyRequestDto dto = new MultiplyRequestDto();
+        dto.setFirst(a);
+        dto.setSecond(b);
+        return dto;
+    }
+
+}
---
+++ webflux-demo/src/test/java/com/reactive/webfluxdemo/Lec06ExchangeTest.java
@@ -0,0 +1,42 @@
+package com.reactive.webfluxdemo;
+
+import com.reactive.webfluxdemo.dto.InputFailedValidationResponse;
+import com.reactive.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class Lec06ExchangeTest extends BaseTest {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Test
+    public void badRequestTest() {
+
+        Mono<Object> responseMono = webClient
+                .get()
+                .uri("reactive-math/square/{number}/throw", 5)
+                .exchangeToMono(this::exchange)
+                .doOnNext(System.out::println)
+                .doOnError(err -> System.out.println(err.getMessage()));
+
+        StepVerifier.create(responseMono)
+                .expectNextCount(1).verifyComplete();
+
+    }
+
+    private Mono<Object> exchange(ClientResponse cr) {
+        if(cr.rawStatusCode() == 400) {
+            return cr.bodyToMono(InputFailedValidationResponse.class);
+        } else {
+            return cr.bodyToMono(Response.class);
+        }
+
+    }
+
+
+}
